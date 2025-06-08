e3dc2093a8c26751f88f9a1c0ecabea784d0b954
AbhisKmr <akaks09@gmail.com>
2025-06-08 19:32:13 +0530

    update: start home

--- ui/src/pages/Netflix.jsx
+++ ui/src/pages/Netflix.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
+import backgroundImage from "../assets/home.jpg";
+import MovieLogo from "../assets/homeTitle.webp";
+import { AiOutlineInfoCircle } from "react-icons/ai";
 
 export default function Netflix() {
   const [isScrolled, setScrolled] = useState(false);
@@ -9,5 +12,12 @@ export default function Netflix() {
     return () => (window.onscroll = null);
   };
 
-  return <Navbar isScrolled={isScrolled} />;
+  return (
+    <div>
+      <Navbar isScrolled={isScrolled} />
+      <div className="hero">
+        <img src={backgroundImage} alt="background" />
+      </div>
+    </div>
+  );
 }
