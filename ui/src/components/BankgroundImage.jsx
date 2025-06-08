6b37e4dd91720064c9716f0bd1530ec8c0dabc29
AbhisKmr <akaks09@gmail.com>
2025-06-08 12:53:09 +0530

    update: signin page ui impov

--- ui/src/components/BankgroundImage.jsx
+++ ui/src/components/BankgroundImage.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import styled from "styled-components";
+import background from "../assets/login.jpg";
 
 export default function BankgroundImage() {
-  return <div>BankgroundImage</div>;
+  return (
+    <Container>
+      <img width={"100%"} src={background} alt="background" />
+    </Container>
+  );
 }
+
+const Container = styled.div``;
--- ui/src/pages/Signup.jsx
+++ ui/src/pages/Signup.jsx
@@ -7,24 +7,39 @@ export default function Signup() {
   return (
     <Container>
       <BackgroundImage />
-      <Header />
-      <div className="body flex column a-center j-center">
-        <div className="text flex column">
-          <h1>Unlimited movies, TV Show and more</h1>
-          <h4>Watch anywhere. Cancel anytime.</h4>
-          <h6>
-            Ready to watch? Enter you email to create or restart your membership
-          </h6>
+      <div className="content">
+        <Header login />
+        <div className="body flex column a-center j-center">
+          <div className="text flex column">
+            <h1>Unlimited movies, TV Show and more</h1>
+            <h4>Watch anywhere. Cancel anytime.</h4>
+            <h6>
+              Ready to watch? Enter you email to create or restart your
+              membership
+            </h6>
+          </div>
+          <div className="form">
+            <input type="email" placeholder="Email Address" name="email" />
+            <input type="password" placeholder="Password" name="password" />
+            <button>Get Started</button>
+          </div>
+          <button>Login</button>
         </div>
-        <div className="form">
-          <input type="email" placeholder="Email Address" name="email" />
-          <input type="password" placeholder="Password" name="password" />
-          <button>Get Started</button>
-        </div>
-        <button>Login</button>
       </div>
     </Container>
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  position: relative;
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    height: 100%;
+    width: 100%;
+    display: grid;
+    grid-template-rows: 15vh 85vh;
+  }
+`;
